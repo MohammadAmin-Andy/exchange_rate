f0002a158925450a802a9ce5df933df1b72a738c
Mamin <pg.mohammadamin@gmail.com>
2024-03-07 11:04:21 -0600

    home Screen Style

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:exchange_rate/constans/constans.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,7 +7,72 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('hi'),
+      backgroundColor: darkGreyColor,
+      appBar: AppBar(
+        backgroundColor: weakGreeenColor,
+        centerTitle: true,
+        title: Text(
+          'نرخ سکه و ارز',
+          style: TextStyle(
+            fontFamily: 'MH',
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(
+                image: AssetImage('images/logo.png'),
+              ),
+              SizedBox(height: 40),
+              _getrateButten(),
+              SizedBox(height: 8),
+              _getcrypto()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getrateButten() {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: softGreenColor,
+        minimumSize: Size(240, 50),
+      ),
+      onPressed: () {},
+      child: Text(
+        'سکه و ارز',
+        style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: blachColor,
+            fontFamily: 'Mh'),
+      ),
+    );
+  }
+
+  Widget _getcrypto() {
+    return OutlinedButton(
+      style: OutlinedButton.styleFrom(
+        side: BorderSide(color: softGreenColor, width: 3),
+        foregroundColor: softGreenColor,
+        minimumSize: Size(240, 50),
+      ),
+      onPressed: () {},
+      child: Text(
+        'ارز دیجیتال',
+        style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: blachColor,
+            fontFamily: 'Mh'),
+      ),
     );
   }
 }
